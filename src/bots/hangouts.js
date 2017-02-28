7b64b9ebf806c60a88729438218aa4b706867639
Marc Seiler <mseiler@gmail.com>
2017-02-28 15:41:22 -0500

    fixed hangouts bot and added a online notification

--- src/bots/hangouts.js
+++ src/bots/hangouts.js
@@ -5,19 +5,21 @@ import uuidV1 from 'uuid/v1';
 const HANGOUTS_EMAIL = process.env.HANGOUTS_EMAIL;
 const HANGOUTS_PASSWORD = process.env.HANGOUTS_PASSWORD;
 const APIAI_TOKEN = process.env.APIAI_TOKEN;
+const MASTER_EMAIL = process.env.GOOGLE_CLOUD_EMAIL; // This should probably be a new env var
 
 const bot = new hangoutsBot(HANGOUTS_EMAIL, HANGOUTS_PASSWORD);
 const app = apiai(APIAI_TOKEN, {language: 'en', requestSource: 'hangouts'});
  
 bot.on('online', () => {
   console.log(`bot logged in as ${HANGOUTS_EMAIL}`);
+  bot.sendMessage(MASTER_EMAIL, 'ALFred Online!');  
 });
 
 var sessionIds = new Map();
 
 bot.on('message', (from, message) => {
   let sender = from.split('/')[0];
-  console.log(sender + ' >> ' + message);
+  console.log(`${sender}(${from})  >> ${message}`);
 
   // Setup a session id if there isn't one
   if (!sessionIds.has(sender)) {
